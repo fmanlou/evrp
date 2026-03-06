e0ae8559526152178a49e0e041b5409007ab9525
fmanlou <2632335513@qq.com>
2026-03-06 23:06:30 +0000

    Add unit tests for input device detection: reject mice without buttons, reject keyboards with incorrect names, and ensure case-insensitive name matching for touchpads.

--- tests/inputdevicetest.cpp
+++ tests/inputdevicetest.cpp
@@ -32,10 +32,33 @@ TEST(InputDevice, DetectMouseFromCapabilities) {
   EXPECT_TRUE(is_mouse_from_capabilities(caps));
 }
 
+TEST(InputDevice, RejectMouseWithoutButtons) {
+  evdev::Capabilities caps = make_base_caps("USB Optical Mouse");
+  caps.ev_rel = true;
+  caps.rel_x = true;
+  caps.rel_y = true;
+  EXPECT_FALSE(is_mouse_from_capabilities(caps));
+}
+
 TEST(InputDevice, DetectKeyboardFromCapabilities) {
   evdev::Capabilities caps = make_base_caps("AT Translated Set 2 keyboard");
   caps.ev_key = true;
   caps.key_enter = true;
   EXPECT_TRUE(is_keyboard_from_capabilities(caps));
+}
+
+TEST(InputDevice, RejectKeyboardWithWrongName) {
+  evdev::Capabilities caps = make_base_caps("generic input device");
+  caps.ev_key = true;
+  caps.key_enter = true;
+  EXPECT_FALSE(is_keyboard_from_capabilities(caps));
+}
+
+TEST(InputDevice, NameMatchIsCaseInsensitive) {
+  evdev::Capabilities caps = make_base_caps("sYnApTiCs tOuChPaD");
+  caps.ev_abs = true;
+  caps.abs_x = true;
+  caps.btn_tool_finger = true;
+  EXPECT_TRUE(is_touchpad_from_capabilities(caps));
 }
 
